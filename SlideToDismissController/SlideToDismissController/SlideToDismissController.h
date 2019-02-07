0e738a15a2f5e03780ea107d32baef098594aa6f
Onur Torna <onurtorna@gmail.com>
2019-02-07 16:22:38 +0300

    Create framework

---
+++ SlideToDismissController/SlideToDismissController/SlideToDismissController.h
@@ -0,0 +1,19 @@
+//
+//  SlideToDismissController.h
+//  SlideToDismissController
+//
+//  Created by Onur Torna on 07.02.2019.
+//  Copyright © 2019 Onur Torna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SlideToDismissController.
+FOUNDATION_EXPORT double SlideToDismissControllerVersionNumber;
+
+//! Project version string for SlideToDismissController.
+FOUNDATION_EXPORT const unsigned char SlideToDismissControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SlideToDismissController/PublicHeader.h>
+
+
